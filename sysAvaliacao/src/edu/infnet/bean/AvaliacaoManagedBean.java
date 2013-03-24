4ec05eb06a29907ee4b823fe0f63ff0024fee4e0
egoulart <emigoulart@gmail.com>
2013-03-23 22:41:02 -0300

    #8 usuario logado no formulario de avaliacao

--- sysAvaliacao/src/edu/infnet/bean/AvaliacaoManagedBean.java
+++ sysAvaliacao/src/edu/infnet/bean/AvaliacaoManagedBean.java
@@ -5,39 +5,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 
 import edu.infnet.dao.AvaliacaoDAO;
-import edu.infnet.dao.AvaliacaoDAOImpl;
 import edu.infnet.model.Avaliacao;
 import edu.infnet.model.Usuario;
 
 @ManagedBean(name="avaliacaoBean")
-@SessionScoped
+@RequestScoped
 public class AvaliacaoManagedBean implements Serializable {
 
 	private static final long serialVersionUID = -4917267200847463255L;
-	
-	private Avaliacao avaliacao= new Avaliacao();
 
-	private final AvaliacaoDAO dao = new AvaliacaoDAOImpl();
+	private Avaliacao avaliacao;
+
+	@ManagedProperty( value = "#{avaliacaoDao}" )
+	private  AvaliacaoDAO avaliacaoDao;
 
 	private List<Avaliacao> lista = new ArrayList<Avaliacao>();
-		
-	public AvaliacaoManagedBean(){
 
-		//setAvaliacao(new Avaliacao());
+	public AvaliacaoManagedBean(){
+		avaliacao= new Avaliacao();
 	}
 
 	public Avaliacao getAvaliacao() {
 		return avaliacao;
 	}
 
+	public AvaliacaoDAO getAvaliacaoDao() {
+		return avaliacaoDao;
+	}
+
 
 	public void setAvaliacao(Avaliacao avaliacao) {
 		this.avaliacao = avaliacao;
 	}
-	
+
 	public List<Avaliacao> getLista() {
 		return lista;
 	}
@@ -47,14 +51,14 @@ public class AvaliacaoManagedBean implements Serializable {
 	}
 
 	public void listarAvaliacoes(Usuario aluno){
-		setLista(dao.consultarAvaliacoesAluno(aluno));
-		
+		setLista(avaliacaoDao.consultarAvaliacoesAluno(aluno));
+
 	}
-	
+
 	public void responderQuestionario(){
-		dao.incluirAvaliacao(avaliacao);
-		
+		avaliacaoDao.incluirAvaliacao(avaliacao);
+
 	}
-	
+
 
 }
